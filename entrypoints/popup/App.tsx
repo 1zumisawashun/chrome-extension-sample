e5895d150bb34fe47c116c96169b08d62b96ac66
1zumisawashun <shun.1zumisawa@gmail.com>
2025-06-20 23:59:32 +0900

    feat:  Add sample background and content scripts

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,10 +1,65 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "@/assets/react.svg";
 import wxtLogo from "/wxt.svg";
 import "./App.css";
+import { COLOR, FONT_SIZE } from "../utils/constants";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [color, setColor] = useState(COLOR.RED);
+  const [fontSize, setFontSize] = useState(FONT_SIZE.M);
+  const [isEnabledStreaming, setIsEnabledStreaming] = useState(false);
+
+  const handleColor = async () => {
+    try {
+      const response = await chrome.runtime.sendMessage({
+        type: "set-color",
+        color: COLOR.RED,
+      });
+      console.log("Color changed to:", response);
+    } catch (error) {
+      console.error("Error setting color:", error);
+    }
+  };
+
+  const handleFontSize = async () => {
+    try {
+      const response = await chrome.runtime.sendMessage({
+        type: "set-font-size",
+        fontSize: FONT_SIZE.L,
+      });
+      console.log("Font size changed to:", response);
+    } catch (error) {
+      console.error("Error setting font size:", error);
+    }
+  };
+
+  const handleIsEnabledStreaming = async () => {
+    try {
+      const response = await chrome.runtime.sendMessage({
+        type: "set-is-enabled-streaming",
+        isEnabledStreaming: true,
+      });
+      console.log("Is enabled streaming changed to:", response);
+    } catch (error) {
+      console.error("Error setting is enabled streaming:", error);
+    }
+  };
+
+  const reset = async () => {
+    await chrome.storage.local.clear();
+  };
+
+  function fetchData() {
+    chrome.storage.local.get(null, (data) => {
+      setColor(data.color);
+      setFontSize(data.fontSize);
+      setIsEnabledStreaming(data.isEnabledStreaming);
+    });
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <>
@@ -17,14 +72,17 @@ function App() {
         </a>
       </div>
       <h1>WXT + React</h1>
+
+      <button onClick={handleColor}>Color: {color}</button>
+      <button onClick={handleFontSize}>Font Size: {fontSize}</button>
+      <button onClick={handleIsEnabledStreaming}>
+        Is Enabled Streaming: {`${isEnabledStreaming}`}
+      </button>
+      <button onClick={reset}>Reset</button>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
-        <p>test</p>
       </div>
       <p className="read-the-docs">
         Click on the WXT and React logos to learn more
--- wxt.config.ts
+++ wxt.config.ts
@@ -1,6 +1,13 @@
-import { defineConfig } from 'wxt';
+import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ['@wxt-dev/module-react'],
+  modules: ["@wxt-dev/module-react"],
+  manifest: {
+    /**
+     * TODO:
+     * scriptingを追加するとどうなるかは未確認
+     * */
+    permissions: ["storage", "scripting", "activeTab", "tabs", "tabCapture"],
+  },
 });
